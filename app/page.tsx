feaaf50abae99dd0e7e373dc53f9e5f6067eb61e
Calvin Nguyen <can005@ucsd.edu>
2023-08-29 18:01:43 -0700

    updated setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
+import type { NextPage } from 'next';
+import '../src/styles/reset.scss';
+
+import About from '../src/sections/About-Projects';
+// here we will compile all the sections of the website together
+const Home: NextPage = () => {
   return (
     <main>
       Welcome to the ACM Projects Website!
+      
+      <About />
     </main>
-  )
-}
+  );
+};
+
+export default Home;
+
---
+++ src/sections/About-Projects/index.tsx
@@ -0,0 +1,11 @@
+import s from '.style.module.scss';
+
+const About: React.FC = () => {
+    return (
+        <h3>
+            ACM Projects is our quarterly projects program where students work in a tight knit team. The program gives students the opportunity to be hands-on outside of courses in fields such as AI, design, and software engineering. The program culminates in a projects showcase and the finished product looks great on resumes. We welcome all skill levels to apply!
+        </h3>
+    );
+};
+
+export default About;
