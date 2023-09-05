f42d3ec8459a27ca6348b9ed23b10e2eb18b8e18
Calvin Nguyen <can005@ucsd.edu>
2023-09-04 17:21:52 -0700

    finished team page
    added grid of photos for each of the project board members
    added logos for hack, design, and ai
    added description for projects

---
+++ src/components/team-card/index.tsx
@@ -0,0 +1,96 @@
+import s from "./style.module.scss";
+import Image from 'next/image'
+
+const default_pic = "/assets/acm_logo.svg"
+
+const project_people = [
+    {
+        name: "Calvin Nguyen",
+        role: "VP Projects",
+        discord: "neniflight",
+        picture: "/assets/team_photos/calvin.jpg",
+        alt: "Calvin Picture"
+    },
+    {
+        name: "Arvin Zhang",
+        role: "Mentor Lead for AI",
+        discord: "arvin._.",
+        picture: "/assets/team_photos/arvin.jpg",
+        alt: "Arvin Picture"
+    },
+    {
+        name: "Nathaniel del Rosario",
+        role: "Mentor Lead for Design",
+        discord: "dimple1216",
+        picture: "/assets/team_photos/nathaniel.jpg",
+        alt: "Nathaniel Picture"
+    },
+    {
+        name: "Sujal Nahata",
+        role: "Mentor Lead for Hack",
+        discord: "real.suj",
+        picture: false,
+        alt: "sujal Picture"
+    },
+    {
+        name: "Jackie Piepkorn",
+        role: "VP AI Events",
+        discord: "devitoinaspeedo",
+        picture: false,
+        alt: "Jackie Picture"
+    },
+    {
+        name: "Cindy Peng",
+        role: "Design TED",
+        discord: "cinpen#3504",
+        picture: false,
+        alt: "Cindy Picture"
+    },
+    {
+        name: "Nicole Gong",
+        role: "Design TED",
+        discord: "lalanicole",
+        picture: false,
+        alt: "Nicole Picture"
+    },
+    {
+        name: "Samantha Sayson",
+        role: "Design TED",
+        discord: "sbds7267",
+        picture: false,
+        alt: "Samantha Picture"
+    }, 
+    {
+        name: "Angela Hu",
+        role: "Hack TED",
+        discord: "angela.__.hu",
+        picture: false,
+        alt: "Angela Picture"
+    },
+    {
+        name: "Khushi Patel",
+        role: "Hack TED",
+        discord: "dolphin2765",
+        picture: false,
+        alt: "Khushi Picture"
+    }
+]
+
+const TeamCards: React.FC = () => {
+    return (
+        <div className={s.container}>
+            {project_people.map((person, index) => (
+                <div className={s.personItem} key={index}>
+                    <img className={s.profilePic} src={person.picture ? person.picture : default_pic} alt={person.alt} />
+                    <div className={s.textContainer}>
+                        <h3 className={s.personName}>{person.name}</h3>
+                        <h5>{person.role}</h5>
+                        <h5>{person.discord}</h5>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default TeamCards;
--- src/sections/About-Projects/index.tsx
+++ src/sections/About-Projects/index.tsx
@@ -1,10 +1,23 @@
-import s from '.style.module.scss';
+import s from './style.module.scss';
+import TeamCards from "../../components/team-card";
 
 const About: React.FC = () => {
     return (
-        <h3>
-            ACM Projects is our quarterly projects program where students work in a tight knit team. The program gives students the opportunity to be hands-on outside of courses in fields such as AI, design, and software engineering. The program culminates in a projects showcase and the finished product looks great on resumes. We welcome all skill levels to apply!
-        </h3>
+        <div className={s.aboutSection}>
+            <div className={s.container}>
+                <img src="/assets/nicole_team.jpg" alt="Project Team"/>
+                <span>
+                    ACM Projects is our quarterly projects program where students work in a tight knit team. The program gives students the opportunity to be hands-on outside of courses in fields such as AI, design, and software engineering. The program culminates in a projects showcase and the finished product looks great on resumes. We welcome all skill levels to apply!
+                </span>
+            </div>
+            <h1>Meet the Team</h1>
+            <TeamCards />
+            <div className={s.logosContainer}>
+                <img src="/assets/ai_name_logo.svg" alt="AI Logo"/>
+                <img src="/assets/design_name_logo.svg" alt="Design Logo"/>
+                <img src="/assets/hack_name_logo.svg" alt="Hack Logo"/>
+            </div>
+        </div>
     );
 };
 
