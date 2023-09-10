79ad4d6ebe85af5392aeb0cdf815aafb528dd20e
Angela Hu <angelahu925@gmail.com>
2023-09-09 23:15:15 -0700

    Created project cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import type { NextPage } from 'next';
 import '../src/styles/reset.scss';
 
-import About from '../src/sections/About-Projects';
+//import About from '../src/sections/About-Projects';
+import Archive from '../src/sections/Archive';
 // here we will compile all the sections of the website together
 const Home: NextPage = () => {
   return (
     <main>
-      Welcome to the ACM Projects Website!
-      
-      <About />
+      <Archive/>
+    
     </main>
   );
 };
