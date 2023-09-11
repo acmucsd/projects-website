ab460b314dd627d74fe39fc3c3964379f722fb95
Calvin Nguyen <can005@ucsd.edu>
2023-09-10 18:12:43 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ import type { NextPage } from 'next';
 import '../src/styles/reset.scss';
 
 import About from '../src/sections/About-Projects';
-import Archive from '../src/sections/Archive';
-import NavigationBar from '../src/components/navbar';
-import TimerHero from '../src/sections/Timer';
+// import Archive from '../src/sections/Archive';
+// import NavigationBar from '../src/components/navbar';
+// import TimerHero from '../src/sections/Timer';
 // here we will compile all the sections of the website together
 const Home: NextPage = () => {
   return (
     <main>
-      <NavigationBar />
-      <TimerHero />
+      {/* <NavigationBar />
+      <TimerHero /> */}
       <About />
-      <Archive/>
+      {/* <Archive/> */}
     </main>
   );
 };
