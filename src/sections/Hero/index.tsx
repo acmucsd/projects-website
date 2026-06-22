1d653a35f107cdb3b40cdc3156a79d39060eadaf
Toby Chin <92127289+spagechi@users.noreply.github.com>
2026-06-22 11:34:58 -0700

    add banner to hero page

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -3,42 +3,51 @@ import Image from "next/image";
 import styles from "./style.module.scss";
 import Description from "../description";
 
+const ProjLogo = "/assets/proj_logo.svg";
+const CountdownImage = "/assets/countdown_image.png";
+
 const Hero = () => {
   const projects_app = "https://acmurl.com/projects-app";
 
   return (
-    <div className={styles.container}>
-      <div className={styles.pageContent}>
-        <div className={styles.hero}>
-          <div className={styles.hero_text}>
-            <h1 className={styles.title}>ACM Projects</h1>
-            <p className={styles.description}>
-              ACM Projects is our quarterly projects program where students work
-              in a tight knit team. The program gives students the opportunity
-              to be hands-on outside of courses in fields such as AI, design,
-              and software engineering. The program culminates in a projects
-              showcase and the finished product looks great on resumes. We
-              welcome all skill levels to apply!
-            </p>
+    <div className={styles.pageContent}>
+      <section className={styles.hero}>
+        <div className={styles.hero_main}>
+          <div className={styles.title}>
+            <Image 
+              className={styles.titleLogo}
+              src={ProjLogo}
+              width={500}
+              height={500}
+              alt="Projects Logo"
+            />
+            <h1 className={styles.titleText}>ACM Projects</h1>
           </div>
-          <img
-            className={styles.image}
-            src="/assets/nicole_team.jpg"
-            alt="ACM Projects"
-          />
+          <p className={styles.description}>
+            ACM Projects is our quarterly projects program where students work
+            in a tight knit team. The program gives students the opportunity
+            to be hands-on outside of courses in fields such as AI, design,
+            and software engineering. The program culminates in a projects
+            showcase and the finished product looks great on resumes. We
+            welcome all skill levels to apply!
+          </p>
           <div className={styles.application}>
             <a href={projects_app} target="_blank">
               <button className={styles.button}>
-                Apply Now!
+                Apply Now
                 {/* Close */}
               </button>
             </a>
-            <div className={styles.deadline}>
-              Applications Due <text>April 2nd, 11:59PM!</text>
-            </div>
           </div>
         </div>
-      </div>
+        <Image
+          className={styles.countdownImage}
+          src={CountdownImage}
+          width={1162}
+          height={1343}
+          alt="Countdown"
+        />
+      </section>
       <Description />
     </div>
   );
