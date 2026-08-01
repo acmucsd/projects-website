22cf77f0d5f8a1c68792ed52e5c07442132984b9
Toby Chin <92127289+spagechi@users.noreply.github.com>
2026-08-01 15:48:10 -0700

    Toby/hero gradient (#35)
    * implement gradient
    
    * fix countdown mobile loading issue where it would initially appear below the apply now button
    
    * fix navbar mobile <-> desktop transition

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -7,6 +7,8 @@ import { Size, useWindowSize } from "../../utils/general";
 
 const ProjLogo = "/assets/proj_logo.svg";
 const CountdownImage = "/assets/countdown_image.png";
+const Gradient = "/assets/hero-gradient.png";
+const GradientMobile = "/assets/hero-gradient-mobile.png";
 
 const Hero = () => {
   const projects_app = "https://acmurl.com/projects-app";
@@ -18,14 +20,22 @@ const Hero = () => {
 
   return (
     <div className={styles.pageContent}>
+      <Image
+        className={styles.gradient}
+        src={mobile ? GradientMobile : Gradient}
+        width={1440}
+        height={1879}
+        alt="Gradient"
+      />
       <section className={styles.hero}>
-        <div className={styles.countdownImageWrapper} style={mobile ? {} : {display: "none"}}>
+        <div className={styles.countdownImageWrapperMobile}>
           <Image
             className={styles.countdownImage}
             src={CountdownImage}
             width={1162}
             height={1343}
             alt="Countdown"
+            priority={true}
           />
           <Countdown className={styles.countdown} />
         </div>
@@ -57,13 +67,14 @@ const Hero = () => {
             </a>
           </div>
         </div>
-        <div className={styles.countdownImageWrapper} style={mobile ? {display: "none"} : {}}>
+        <div className={styles.countdownImageWrapper}>
           <Image
             className={styles.countdownImage}
             src={CountdownImage}
             width={1162}
             height={1343}
             alt="Countdown"
+            priority={true}
           />
           <Countdown className={styles.countdown} />
         </div>
