83bdfc6845f9f136321c5b3ddf225ed9f1c35926
Nathan Liu <nathanliu17@gmail.com>
2026-09-23 12:09:35 -0700

    add: about page header, justify fixes

--- src/sections/About-Projects/index.tsx
+++ src/sections/About-Projects/index.tsx
@@ -11,13 +11,17 @@ const About: React.FC = () => {
         <div className={s.aboutSection} id="about">
             <div className={s.container}>
               <div>
-                <Image src="/assets/about_us_stock.png" width={1253} height={357} alt="ACM Projects" className={s.carousel_img}/>
+                <div className={s.banner}>
+                    <Image src="/assets/about_us_stock.png" width={1253} height={357} alt="ACM Projects" className={s.carousel_img}/>
+                </div>
                 <h1>About ACM Projects</h1>
                 <p>
                   ACM Projects is our quarterly projects program where students work in a tight knit team. The program gives students the opportunity to be hands-on outside of courses in fields such as AI, design, and software engineering. The program culminates in a projects showcase and the finished product looks great on resumes. We welcome all skill levels to apply!                </p>
               </div>
-              <h1>Meet the Team</h1>
-              <TeamCards />
+              <div className={s.teamContainer}>
+                <h1>Meet our Team</h1>
+                <TeamCards />
+              </div>
             </div>
         </div>
     );
